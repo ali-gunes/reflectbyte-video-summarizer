293f9fc985265018e1f0c2b31346d21031dbe46a
Ali Güneş <agunes372@gmail.com>
2024-04-15 08:52:04 +0300

    Add pages, include settings

--- backend/reflectbyte/reflectbyte/settings.py
+++ backend/reflectbyte/reflectbyte/settings.py
@@ -82,6 +82,17 @@ DATABASES = {
 }
 
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'postgres',
+#         'USER': 'postgres',
+#         'PASSWORD': 'RediDad42.',
+#         'HOST': 'localhost',
+#         'PORT': '5432',
+#     }
+# }
+
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
